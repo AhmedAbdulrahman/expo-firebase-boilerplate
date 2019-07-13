f32807d915025c165933418b382ffc17b855b8ed
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-07-13 16:25:26 +0200

    :wrench: Update Babel config to use module resolver for aliases

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@components': './components',
+            '@containers': './containers',
+            '@screens': './screens',
+            '@theme': './theme',
+            '@constants': './constants',
+            '@navigation': './navigation',
+            '@theme': './theme',
+            '@assets': './assets',
+          },
+        },
+      ],
+    ],
   };
 };

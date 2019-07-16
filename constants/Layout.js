073cd97405fcf6f9b3fe18e07d82d4502634a82d
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-07-16 11:48:09 +0200

    :wrench: Ratio util

--- constants/Layout.js
+++ constants/Layout.js
@@ -3,10 +3,28 @@ import { Dimensions } from 'react-native';
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 
+let calRatio = width <= height ? 16 * (width / height) : 16 * (height / width);
+if (width <= height) {
+  if (calRatio < 9) {
+    calRatio = width / 9;
+  } else {
+    calRatio = height / 18;
+  }
+} else {
+  if (calRatio < 9) {
+    calRatio = height / 9;
+  } else {
+    calRatio = width / 18;
+  }
+}
+
+const ratio = calRatio / (360 / 9);
+
 export default {
   window: {
     width,
     height,
+    ratio,
   },
   isSmallDevice: width < 375,
 };

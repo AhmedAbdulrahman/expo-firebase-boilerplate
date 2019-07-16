9ec6ffbcffaf8f523bb9120040d760681e89deb5
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-07-16 11:02:56 +0200

    :zap: Add navigation screens constant

---
+++ navigation/constants.js
@@ -0,0 +1,13 @@
+export const screens = {
+  auth: 'Auth',
+  authLoading: 'AuthLoading',
+  app: 'App',
+  home: 'Home',
+  detail: 'Detail',
+  signIn: 'SignIn',
+  signUp: 'SignUp',
+  welcome: 'Welcome',
+  drawer: 'DrawerNavigator',
+  settings: 'Settings',
+  profile: 'Profile',
+};

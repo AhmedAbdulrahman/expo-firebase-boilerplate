ef5a0e40ddb7a1124eb28d1cbced80bf84c5a0ad
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-07-18 10:39:17 +0200

    :zap: Add AsyncStorage utils

---
+++ storage/settingsStorage.js
@@ -0,0 +1,28 @@
+import { AsyncStorage } from 'react-native';
+
+const STORAGE_KEY = 'SETTINGS';
+
+const DEFAULT_SETTINGS = {
+  name: '',
+  locale: 'en',
+};
+
+export const loadSettings = async () => {
+  try {
+    const settings = await AsyncStorage.getItem(STORAGE_KEY);
+    if (settings === null) {
+      return DEFAULT_SETTINGS;
+    }
+    return JSON.parse(settings);
+  } catch (error) {
+    console.log('Error loading settings', error);
+  }
+};
+
+export const saveSettings = async settings => {
+  try {
+    AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(settings));
+  } catch (error) {
+    console.log('Error loading settings', error);
+  }
+};

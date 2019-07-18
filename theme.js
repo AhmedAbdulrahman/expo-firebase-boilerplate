dc49a4f6b025f1bbd0ed780b4fa1ac89fadceb40
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-07-18 19:33:32 +0200

    :art: Add global theme

---
+++ theme.js
@@ -0,0 +1,19 @@
+export default theme = {
+  font: {
+    cera: {
+      900: 'space-mono',
+      700: 'space-mono',
+      500: 'space-mono',
+      400: 'space-mono',
+      300: 'space-mono',
+      100: 'space-mono',
+    },
+  },
+  palette: {
+    common: {
+      black: '#1E1E23',
+      white: '#FFFFFF',
+      grey: '#BBBBBD',
+    },
+  },
+};

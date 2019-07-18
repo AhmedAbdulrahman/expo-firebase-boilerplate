66ea2956fb6c50273cbd6f4f7bd5de2d2b456531
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-07-18 10:43:57 +0200

    :wrench: Update SettingsStack

--- navigation/SettingsStack.js
+++ navigation/SettingsStack.js
@@ -1,15 +1,32 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 import MenuDrawerButton from '@components/MenuDrawerButton';
-import { Settings } from '@screens';
+import { Settings, LanguageSelector, About } from '@screens';
 
 const SettingsStack = createStackNavigator({
   Settings: {
     screen: Settings,
-    navigationOptions: {
-      headerTitle: 'Settings',
-      headerLeft: MenuDrawerButton,
-    },
+    navigationOptions: ({ screenProps: { t, locale } }) => ({
+      headerTitle: t('navigation.settings'),
+      ...(locale === 'ar'
+        ? {
+            headerRight: <MenuDrawerButton />,
+            headerRightContainerStyle: {
+              paddingRight: 10,
+            },
+          }
+        : {
+            headerLeft: <MenuDrawerButton />,
+            headerLeftContainerStyle: {
+              paddingLeft: 10,
+            },
+          }),
+      // headerLeft: locale === 'en' ? <MenuDrawerButton /> : null,
+      // headerRight: locale === 'ar' ? <MenuDrawerButton /> : null,
+    }),
   },
+  LanguageSelector: LanguageSelector,
+  About: About,
 });
 
 export default SettingsStack;

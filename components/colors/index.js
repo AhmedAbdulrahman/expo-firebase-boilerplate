b8fb2c0e1f5074c54205e3ecf8037c0593d72520
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-07-28 10:42:45 +0200

    :art: Add basic color palette

---
+++ components/colors/index.js
@@ -0,0 +1,8 @@
+export { default as blue } from './blue';
+export { default as common } from './common';
+export { default as green } from './green';
+export { default as grey } from './grey';
+export { default as pink } from './pink';
+export { default as purple } from './purple';
+export { default as red } from './red';
+export { default as yellow } from './yellow';
---
+++ components/colors/yellow.js
@@ -0,0 +1,18 @@
+const yellow = {
+  50: '',
+  100: '',
+  200: '#FFFFE6',
+  300: '#FFFFCD',
+  400: '#FAFFB4',
+  500: '#FAFF9B',
+  600: '#F0EA66',
+  700: '#ECE552',
+  800: '',
+  900: '',
+  A100: '',
+  A200: '',
+  A400: '#FFFA5D',
+  A700: '',
+};
+
+export default yellow;

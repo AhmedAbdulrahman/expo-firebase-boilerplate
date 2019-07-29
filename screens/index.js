6d07ef8f61be2bfbf66874f8f84bf74ea06c4355
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-07-29 23:52:11 +0200

    :fire: Remove welcome screen

--- screens/index.js
+++ screens/index.js
@@ -3,21 +3,9 @@ import SignUp from './SignUp';
 import AuthLoading from './AuthLoading';
 import Home from './Home';
 import Detail from './Detail';
-import Welcome from './Welcome';
 import Profile from './Profile';
 import Settings from './Settings';
 import LanguageSelector from './LanguageSelector';
 import About from './About';
 
-export {
-  SignIn,
-  SignUp,
-  AuthLoading,
-  Home,
-  Welcome,
-  Profile,
-  Settings,
-  Detail,
-  LanguageSelector,
-  About,
-};
+export { SignIn, SignUp, AuthLoading, Home, Profile, Settings, Detail, LanguageSelector, About };

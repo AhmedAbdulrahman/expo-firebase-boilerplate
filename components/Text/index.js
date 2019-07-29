70b59ca0bb7e48aa76fc878e31bce11fecd841df
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-07-29 23:58:41 +0200

    :wrench: Update Text component

--- components/Text/index.js
+++ components/Text/index.js
@@ -1,35 +1,38 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
-const TextUI = styled.Text`
-  font-family: ${({ theme }) => theme.font.cera[400]};
-`;
+const TextUI = styled.Text``;
 
 export const Label = styled(TextUI)`
-  font-family: ${({ theme }) => theme.font.cera[700]};
   font-size: 11;
   line-height: ${() => 11 * 1.2};
-  letter-spacing: ${() => 11 * 0.1};
+  letter-spacing: ${() => 9 * 0.1};
   text-transform: uppercase;
   color: ${({ theme, focused }) =>
-    focused ? theme.palette.common.black : theme.palette.common.grey};
+    focused ? theme.palette.primary.accent : theme.palette.text.hint};
+  margin-bottom: 4px;
+  ${props =>
+    props.center &&
+    css`
+      text-align: center;
+    `}
 `;
 
-export const CaptionRegular = styled(TextUI)`
-  font-size: 12;
-  line-height: ${() => 12 * 1.15};
-  letter-spacing: ${() => 12 * 0.02};
+export const Body = styled(TextUI)`
+  font-size: 16;
+  line-height: ${() => 16 * 1.15};
+  letter-spacing: ${() => 16 * 0.02};
+  color: ${({ theme }) => theme.palette.text.primary};
 `;
 
-export const CaptionBold = styled(CaptionRegular)`
-  font-family: ${({ theme }) => theme.font.cera[700]};
+export const BodyBold = styled(Body)`
+  text-decoration: underline;
 `;
 
 export const HeaderMain = styled.Text`
-  font-family: ${({ theme }) => theme.font.cera[500]};
   font-size: 26;
   line-height: 26;
   letter-spacing: ${() => 26 * 0.1};
-  color: ${({ theme }) => theme.palette.common.white};
+  color: ${({ theme }) => theme.palette.text.primary};
   text-transform: uppercase;
   text-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
   text-align: center;

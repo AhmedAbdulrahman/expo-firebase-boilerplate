d57181e8c69c8f85ccb3b4eed8610c16a195cf05
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-08-01 00:55:54 +0200

    :wrench: Make Home screen available only for authorization users

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,36 @@
 /* eslint-disable react-native/no-raw-text */
 import React from 'react';
+import { View } from 'react-native';
+import { compose } from 'recompose';
 import Container from '@components/Container';
 import { HeaderMain, Body, BodyBold } from '@components/Text';
 import { screens } from '@navigation/constants';
+import { withAuthorization } from '../hocs/sessions';
+import AuthUserContext from '../hocs/sessions/context';
 
 const Home = ({ navigation, screenProps: { t, locale } }) => (
-  <Container>
-    <HeaderMain>{t('home.message')}</HeaderMain>
-    <BodyBold onPress={() => navigation.navigate(screens.detail)}>{t('home.go')}</BodyBold>
-    <Body>Current locale: {locale}. </Body>
-  </Container>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <Container>
+        <HeaderMain>{t('home.message')}</HeaderMain>
+        <BodyBold onPress={() => navigation.navigate(screens.detail)}>{t('home.go')}</BodyBold>
+        <Body>Current locale: {locale}. </Body>
+        <View style={{ marginTop: 40 }}>
+          <BodyBold style={{ fontSize: 20 }}>Firebase Current User</BodyBold>
+          <Body>Email: {authUser.email}. </Body>
+          <Body>Email Verified: {authUser.emailVerified}. </Body>
+          <Body>Username: {authUser.username}. </Body>
+        </View>
+        {authUser && authUser.roles && authUser.roles.ADMIN && (
+          <View style={{ marginTop: 140 }}>
+            <BodyBold>User Role is : {authUser.roles.ADMIN}</BodyBold>
+            <Body style={{ fontSize: 20 }}>Admin Area</Body>
+          </View>
+        )}
+      </Container>
+    )}
+  </AuthUserContext.Consumer>
 );
 
-export default Home;
+const condition = authUser => !!authUser;
+export default compose(withAuthorization(condition))(Home);

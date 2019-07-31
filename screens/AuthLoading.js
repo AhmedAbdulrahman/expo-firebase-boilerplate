89f3b0e656c500c8038ad51f8be6a0eeaf56a710
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-08-01 00:48:07 +0200

    :wrench: Set setTimeout before navigating between home and signin screens

--- screens/AuthLoading.js
+++ screens/AuthLoading.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ActivityIndicator, AsyncStorage, StatusBar, StyleSheet, View } from 'react-native';
+import { screens } from '@navigation/constants';
 
 class AuthLoadingScreen extends React.Component {
   constructor() {
@@ -9,11 +10,13 @@ class AuthLoadingScreen extends React.Component {
 
   // Fetch the token from storage then navigate to our appropriate place
   _bootstrapAsync = async () => {
-    const userToken = await AsyncStorage.getItem('userToken');
+    const userToken = await AsyncStorage.getItem('authUser');
 
     // This will switch to the App screen or Auth screen and this loading
     // screen will be unmounted and thrown away.
-    this.props.navigation.navigate(userToken ? 'App' : 'SignIn');
+    setTimeout(() => {
+      this.props.navigation.navigate(userToken ? screens.home : screens.signIn);
+    }, 500);
   };
 
   render() {
